a7937df2f1a148d8ba98ed62b9dda807aa344041
Peter White <pwhite2@gmail.com>
2020-03-10 12:32:58 -0700

    [STS-3] Added Google Analytics support

---
+++ package_for_web_store.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+VERSION="0.1.1"
+
+# Replace version number if not already done so...
+gsed -i "s/\"version\": \".*\"/\"version\": \"$VERSION\"/g" src/manifest.json
+
+# Replace Google Analytics Tracking id with value stored in local environment variable so folks don't unintentionally track forked extensions using my ID...
+gsed -i s/UA-XXXXXXXXX-X/$STS_GA_TRACKING_ID/g src/analytics.js
+
+# Zip src dir for upload to the Chrome Web Store
+zip -r simple-tab-sorter-v$VERSION.zip src
+
+# Revert tracking id change to src/analytics.js to prevent if from being unintentionally committed to GitHub
+git checkout HEAD -- src/analytics.js
