d105a4a8f76138f0fc89b5ecfb9ce368a02efe34
Peter White <pwhite@riffyn.com>
2021-02-21 18:26:44 -0800

    Sts 11 (#12)
    * [STS-11] Added support for STS derived works & removed analytics code

--- src/options.js
+++ src/options.js
@@ -1,15 +1,19 @@
+const THE_GREAT_SUSPENDER_EXTENSION_ID = "klbibkeccnjlkjkiokjodocebajanakg";
+
 // Save options to chrome.storage
 function saveOptions() {
     var sortBy = document.getElementById('sortBy').value;
     var groupFrom = document.getElementById('groupFrom').value;
     var preserveOrderWithinGroups = document.getElementById('preserveOrderWithinGroups').checked;
     var groupSuspendedTabs = document.getElementById('groupSuspendedTabs').checked;
+    var tabSuspenderExtensionId = document.getElementById('tabSuspenderExtensionId').value;
     var sortPinnedTabs = document.getElementById('sortPinnedTabs').checked;
     chrome.storage.sync.set({
         sortBy: sortBy,
         groupFrom: groupFrom,
         preserveOrderWithinGroups: preserveOrderWithinGroups,
         groupSuspendedTabs: groupSuspendedTabs,
+        tabSuspenderExtensionId: tabSuspenderExtensionId,
         sortPinnedTabs: sortPinnedTabs
     }, function () {
         document.getElementById('save').setAttribute("disabled", true);
@@ -27,6 +31,7 @@ function restoreOptions() {
         groupFrom: 'leftToRight',
         preserveOrderWithinGroups: true,
         groupSuspendedTabs: false,
+        tabSuspenderExtensionId: THE_GREAT_SUSPENDER_EXTENSION_ID,
         sortPinnedTabs: false
     }, function (items) {
         toggleTabGroupOptions(items.sortBy);
@@ -34,6 +39,7 @@ function restoreOptions() {
         document.getElementById('groupFrom').value = items.groupFrom;
         document.getElementById('preserveOrderWithinGroups').checked = items.preserveOrderWithinGroups;
         document.getElementById('groupSuspendedTabs').checked = items.groupSuspendedTabs;
+        document.getElementById('tabSuspenderExtensionId').value = items.tabSuspenderExtensionId;
         document.getElementById('sortPinnedTabs').checked = items.sortPinnedTabs;
     });
 }
@@ -44,12 +50,14 @@ function toggleSaveButton() {
         groupFrom: 'leftToRight',
         preserveOrderWithinGroups: true,
         groupSuspendedTabs: false,
+        tabSuspenderExtensionId: THE_GREAT_SUSPENDER_EXTENSION_ID,
         sortPinnedTabs: false
     }, function (items) {
         if (document.getElementById('sortBy').value != items.sortBy ||
             document.getElementById('groupFrom').value != items.groupFrom ||
             document.getElementById('preserveOrderWithinGroups').checked != items.preserveOrderWithinGroups ||
             document.getElementById('groupSuspendedTabs').checked != items.groupSuspendedTabs ||
+            (document.getElementById('tabSuspenderExtensionId').value != items.tabSuspenderExtensionId && document.getElementById('tabSuspenderExtensionId').value != THE_GREAT_SUSPENDER_EXTENSION_ID ) ||
             document.getElementById('sortPinnedTabs').checked != items.sortPinnedTabs) {
             document.getElementById('save').removeAttribute("disabled");
             // Hide status to reflect that changes have not been saved
@@ -59,6 +67,15 @@ function toggleSaveButton() {
             document.getElementById('save').setAttribute("disabled", true);
         }
     });
+}
+
+function toggleTabSuspenderExtensionId() {
+    if (document.getElementById('groupSuspendedTabs').checked) {
+        document.getElementById('tabSuspenderExtensionId').setAttribute("disabled", true);
+    } else {
+        document.getElementById('tabSuspenderExtensionId').removeAttribute("disabled");
+    }
+    toggleSaveButton();
 }
 
 function toggleTabGroupOptions(sortBy) {
@@ -80,8 +97,14 @@ $("#settings-form").submit(function(e) {
 document.getElementById('sortBy').addEventListener('change', function() {
     toggleTabGroupOptions(this.value);
 });
+
 document.getElementById('groupFrom').addEventListener('change', toggleSaveButton);
 document.getElementById('preserveOrderWithinGroups').addEventListener('change', toggleSaveButton);
-document.getElementById('groupSuspendedTabs').addEventListener('change', toggleSaveButton);
+document.getElementById('groupSuspendedTabs').addEventListener('change', toggleTabSuspenderExtensionId);
+document.getElementById('tabSuspenderExtensionId').addEventListener('input', toggleSaveButton);
 document.getElementById('sortPinnedTabs').addEventListener('change', toggleSaveButton);
 document.getElementById('save').addEventListener('click', saveOptions);
+
+$(document).ready(function() {
+    toggleTabSuspenderExtensionId();
+});
